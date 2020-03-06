61dfed8a80401c8d041a9a51d2d370487782c38c
mike <>
2020-03-06 14:08:03 +0000

    Kareoke bar has name, room is empty, add guests to room and songs, tests passed

--- kareoke_bar.rb
+++ kareoke_bar.rb
@@ -4,5 +4,31 @@ attr_reader :name
 
 def initialize(name)
   @name = name
+
+  @room1 = []
+  @room2 = []
+  @songs = []
 end
+
+def room_count()
+  return @room1.length()
+end
+
+
+def add_guest(guest)
+  @room1.push(guest)
+  return @room1.length()
+end
+
+def song_count()
+  return @songs.length()
+end
+
+def add_song(song)
+  @songs.push(song)
+  return @songs.length
+end
+
+def hire_room()
+  if
 end
--- specs/kareoke_bar_specs.rb
+++ specs/kareoke_bar_specs.rb
@@ -11,17 +11,38 @@ class Kareoke_barTest < Minitest::Test
 
   def setup()
     @kareoke_bar = Kareoke_bar.new("Big Swinging Hits")
-    @room1 = Room.new(10)
-    @room2 = Room.new(15)
     @guest1 = Guest.new("Clive", 30)
     @guest2 = Guest.new("Jane", 20)
     @song1 = Song.new("Aeroplane", "Red Hot Chili Peppers")
     @song2 = Song.new("Fast Car", "Tracy Chapman")
+
+    @songs = [@song1, @song2]
   end
 
   def test_kareoke_bar_has_name()
     assert_equal("Big Swinging Hits", @kareoke_bar.name)
 end
 
+def test_room_is_empty()
+  assert_equal(0, @kareoke_bar.room_count())
+end
+
+  def test_add_guest_to_room()
+    @kareoke_bar.add_guest(@guest1)
+    assert_equal(1, @kareoke_bar.room_count())
+end
+
+def test_song_count_is_zero()
+  assert_equal(0, @kareoke_bar.song_count())
+end
+
+def test_add_song_to_room()
+  @kareoke_bar.add_song(@song1)
+  assert_equal(1, @kareoke_bar.song_count())
+end
+
+def test_can_guest_hire_room()
+  @kareoke_bar.hire_room(@room1)
+  assert_equal(true, @kareoke_bar.fire_room())
 
 end
